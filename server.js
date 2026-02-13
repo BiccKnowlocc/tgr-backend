b63113997999cf9601a9f4cf312f85f8925d775b
Nicholas Bullock <nickb@tobermorygroceryrun.ca>
2026-02-13 17:34:23 -0500

    Fix User model import

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("./models/User");
 
 const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 
